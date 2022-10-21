da110f5773056883d3efe0d9480eed0b61a3f2c9
jeaun80 <kims1706@naver.com>
2022-10-21 17:22:41 +0900

    프로젝트시작

---
+++ src/test/java/com/example/reflix/ReflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
