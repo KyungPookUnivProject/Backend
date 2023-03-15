f366a31aed171a57abcb751265b89a59f74eb019
jeaun80 <kims1706@naver.com>
2023-03-15 17:07:16 +0900

    test

--- src/main/java/com/example/reflix/service/userService.java
+++ src/main/java/com/example/reflix/service/userService.java
@@ -19,8 +19,8 @@ public class userService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
     private final user1Repository user1Repository;
-//    public String login(userLoginDto userLoginDto){
-//
+    public String login(userLoginDto userLoginDto){
+
 //        user member = user1Repository.findByEmail(userLoginDto.getEmail())
 //                .orElseThrow(() -> new IllegalArgumentException("노가입"));
 //
@@ -33,43 +33,44 @@ public class userService {
 //        else{
 //            return null;
 //        }
-//        //dto에서 id pwd 구분해서 만든다음 디비에있는 id와같은지 검사후 같으면
-//        //비번검사 비번이 인코딩해서 디비에서 인코딩된 비번과 같으면 true리턴
-//    }
-//
-//    public String register(userRegisterDto userRegisterDto){
-//        String hashPW=bCryptPasswordEncoder.encode(userRegisterDto.getPassword());
-//        user member = user.builder()
-//                .name(userRegisterDto.getName())
-//                .password(hashPW)
-//                .email(userRegisterDto.getEmail())
-//                .Role(Role.USER)
-//                .build();
-//        if(user1Repository.findByEmail(userRegisterDto.getEmail()).isPresent()){
-//            return "overlap";
-//        }
-//        else{
-//            return user1Repository.save(member).getEmail().toString();
-//        }
-//
-//        //유저 회원가입
-//        //dto에서 이메일,아이디,비번 이름을 받는다
-//        //디비에서 이메일이 겹치는지 확인
-//        //이메일이 겹치지않으면 이에일 아이디 저장 비밀번호 인코딩후 저장 이름 저장
-//        //잘되면 됐다고 리턴 오류나면 왜오류인지 리턴
-//    }
-//
-//    public userDetailResponseDto userDetail(String email){
-//        Optional<user> Optionaluser = user1Repository.findByEmail(email);
-//        user user = Optionaluser.orElseThrow(UnsupportedOperationException::new);
-//        userDetailResponseDto resposnedto = userDetailResponseDto.builder()
-//                .email(user.getEmail())
-//                .name(user.getName())
-//                .build();
-//        return resposnedto;
-//    }
-//
-//    public void mypage(String email){
-//
-//    }
+        //dto에서 id pwd 구분해서 만든다음 디비에있는 id와같은지 검사후 같으면
+        //비번검사 비번이 인코딩해서 디비에서 인코딩된 비번과 같으면 true리턴
+        return null;
+    }
+
+    public String register(userRegisterDto userRegisterDto){
+        String hashPW=bCryptPasswordEncoder.encode(userRegisterDto.getPassword());
+        user member = user.builder()
+                .name(userRegisterDto.getName())
+                .password(hashPW)
+                .email(userRegisterDto.getEmail())
+                .Role(Role.USER)
+                .build();
+        if(user1Repository.findByEmail(userRegisterDto.getEmail()).isPresent()){
+            return "overlap";
+        }
+        else{
+            return user1Repository.save(member).getEmail().toString();
+        }
+
+        //유저 회원가입
+        //dto에서 이메일,아이디,비번 이름을 받는다
+        //디비에서 이메일이 겹치는지 확인
+        //이메일이 겹치지않으면 이에일 아이디 저장 비밀번호 인코딩후 저장 이름 저장
+        //잘되면 됐다고 리턴 오류나면 왜오류인지 리턴
+    }
+
+    public userDetailResponseDto userDetail(String email){
+        Optional<user> Optionaluser = user1Repository.findByEmail(email);
+        user user = Optionaluser.orElseThrow(UnsupportedOperationException::new);
+        userDetailResponseDto resposnedto = userDetailResponseDto.builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+        return resposnedto;
+    }
+
+    public void mypage(String email){
+
+    }
 }
--- src/main/java/com/example/reflix/web/controller/userController.java
+++ src/main/java/com/example/reflix/web/controller/userController.java
@@ -29,80 +29,84 @@ public class userController {
     private final user1Repository user1Repository;
     private final JwtTokenProvider jwtTokenProvider;
     ///로그인 테스트 코드 ///
-//
-//    @PostMapping("/join")
-//    public String join(){
-//        user user = com.example.reflix.web.domain.user.builder()
-//                .email("jeaun80@naver.com")
-//                .name("jingwon")
-//                .password("hi")
-//                .Role(Role.USER)
-//                .build();
-//        user1Repository.save(user);
-//        return user.getEmail();
-//    }
-//
-//    ///로그인 테스트코드///
-//    //로그인 아이디 비번 보내고 검증하는단계 추후 소셜로그인으로 변경or추가
-//    @PostMapping("/auth/login")
-//    public ResponseEntity login(@RequestBody userLoginDto userLoginDto){
-//
-//
-//        log.info(userLoginDto.getEmail());
-//        String token = userservice.login(userLoginDto);
-//
-//        log.info(token);
-//        if(token==null){
-//            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-//        }
-//        else{
-//            HttpHeaders responseHeaders = new HttpHeaders();
-//            responseHeaders.set("Authorization",token);
-//            return new ResponseEntity(token,responseHeaders,HttpStatus.OK);
-//        }
-//
-//        //로그인 후 이유저가 취향 디비가 있는지검사
-//        //잇으면 content/표시로 redirect
-//        //없으면 프론트단에서 취향고르는 페이지로 redirect하게함
-//
-//        //여기서는 db에 취향디비가 있는지만 검사해서 있으면 있다고 리턴
-//        //없으면 없다고리턴
-//        //프론트단에서 리턴결과 확인해서 redirect어디로 할지 결정
-//    }
-//
-//    //회원가입 회원정보 db에 넣기 추후 소셜로그인 대비해서 유저이메일 받기 이메일을 유니크키로 쓰자. 기본키는 고유번호다.
-//    @PostMapping("/auth/register")
-//    public ResponseEntity register(@RequestBody userRegisterDto userRegisterDto){
-//
-//        //회원가입 이 성공적으로 끝낫는지 아닌지 리턴
-//        //리턴 결과르 프론트에서 보고 메인페이지로 갈지
-//        //뭐땜에 회원가입안됐다고 할지 리턴결과로 메시지표시
-//        String responseEmail= userservice.register(userRegisterDto);
-//        if(responseEmail.equals("overlap")){
-//            String body = "중복된 이메일이 있습니다.";
-//            return new ResponseEntity(body,HttpStatus.BAD_REQUEST);
-//        }
-//        else{
-//            return new ResponseEntity(responseEmail,HttpStatus.OK);
-//        }
-//    }
-//
-//    @GetMapping("/user/detail/{email}")
-//    public ResponseEntity userDetail(String email){
-//
-//        userDetailResponseDto dto = userservice.userDetail(email);
-//        return new ResponseEntity(dto,HttpStatus.OK);
-//    }
-//    //로그아웃 어떻게 구현해야되는지 모르겟다. 진짜로 시큐리티 사용해야될거같은데
-//    @GetMapping("/logout")
-//    public ResponseEntity<Void> logout(){
-//
-//        return new ResponseEntity("hi", HttpStatus.CREATED);
-//    }
-//    @GetMapping("/test")
-//    public String test(){
-//        return "통과해라 제발";
-//    }
-//
-//    //로그인 - > 회원가입 정보입력화면은 정적이기 때문에 프론트단에서 처리한다. rest
+
+    @GetMapping("/auth/fi")
+    public String test1(){
+        return "hi";
+    }
+    @PostMapping("/join")
+    public String join(){
+        user user = com.example.reflix.web.domain.user.builder()
+                .email("jeaun80@naver.com")
+                .name("jingwon")
+                .password("hi")
+                .Role(Role.USER)
+                .build();
+        user1Repository.save(user);
+        return user.getEmail();
+    }
+
+    ///로그인 테스트코드///
+    //로그인 아이디 비번 보내고 검증하는단계 추후 소셜로그인으로 변경or추가
+    @PostMapping("/auth/login")
+    public ResponseEntity login(@RequestBody userLoginDto userLoginDto){
+
+
+        log.info(userLoginDto.getEmail());
+        String token = userservice.login(userLoginDto);
+
+        log.info(token);
+        if(token==null){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+        else{
+            HttpHeaders responseHeaders = new HttpHeaders();
+            responseHeaders.set("Authorization",token);
+            return new ResponseEntity(token,responseHeaders,HttpStatus.OK);
+        }
+
+        //로그인 후 이유저가 취향 디비가 있는지검사
+        //잇으면 content/표시로 redirect
+        //없으면 프론트단에서 취향고르는 페이지로 redirect하게함
+
+        //여기서는 db에 취향디비가 있는지만 검사해서 있으면 있다고 리턴
+        //없으면 없다고리턴
+        //프론트단에서 리턴결과 확인해서 redirect어디로 할지 결정
+    }
+
+    //회원가입 회원정보 db에 넣기 추후 소셜로그인 대비해서 유저이메일 받기 이메일을 유니크키로 쓰자. 기본키는 고유번호다.
+    @PostMapping("/auth/register")
+    public ResponseEntity register(@RequestBody userRegisterDto userRegisterDto){
+
+        //회원가입 이 성공적으로 끝낫는지 아닌지 리턴
+        //리턴 결과르 프론트에서 보고 메인페이지로 갈지
+        //뭐땜에 회원가입안됐다고 할지 리턴결과로 메시지표시
+        String responseEmail= userservice.register(userRegisterDto);
+        if(responseEmail.equals("overlap")){
+            String body = "중복된 이메일이 있습니다.";
+            return new ResponseEntity(body,HttpStatus.BAD_REQUEST);
+        }
+        else{
+            return new ResponseEntity(responseEmail,HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/user/detail/{email}")
+    public ResponseEntity userDetail(String email){
+
+        userDetailResponseDto dto = userservice.userDetail(email);
+        return new ResponseEntity(dto,HttpStatus.OK);
+    }
+    //로그아웃 어떻게 구현해야되는지 모르겟다. 진짜로 시큐리티 사용해야될거같은데
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(){
+
+        return new ResponseEntity("hi", HttpStatus.CREATED);
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "통과해라 제발";
+    }
+
+    //로그인 - > 회원가입 정보입력화면은 정적이기 때문에 프론트단에서 처리한다. rest
 }
